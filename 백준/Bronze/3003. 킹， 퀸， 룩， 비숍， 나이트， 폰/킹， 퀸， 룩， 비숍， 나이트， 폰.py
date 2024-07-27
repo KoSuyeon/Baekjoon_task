5b3bf4a1ed99b54af88ede7d4ed688d9c94ba3f7
kko_ba <90381286+KoSuyeon@users.noreply.github.com>
2024-07-28 07:20:41 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.py

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,3 +1,15 @@
 k, q, l, b, n, p = map(int,input().split())
 
 print(1-k,1-q,2-l,2-b,2-n,8-p)
+
+"""
+n=-1
+for i in input().split():print(106%n+1-int(i));n+=2
+
+#106 % 1은 0
+#106 % 3은 1
+#106 % 5는 1
+#106 % 7은 1
+#106 % 9는 7
+#106 % 11은 7
+"""
