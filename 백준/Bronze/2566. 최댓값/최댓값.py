41431dddb5af61954d9ad906e16171d8bd9b8491
kko_ba <90381286+KoSuyeon@users.noreply.github.com>
2024-07-28 08:25:37 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,11 @@
+n=[list(map(int,input().split())) for _ in range(9)]
+m=max(map(max,n))
+
+for i in range(9):
+    for j in range(9):
+        if n[i][j] == m:
+            max_pos=(i+1,j+1)
+            break
+
+print(m)     
+print(max_pos[0],max_pos[1])
