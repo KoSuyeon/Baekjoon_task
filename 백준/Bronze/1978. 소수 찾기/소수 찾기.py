87ca030dd8fd25e440058b68df31af75394b2c77
kko_ba <90381286+KoSuyeon@users.noreply.github.com>
2024-08-04 09:15:01 +0900

    Update 소수 찾기.py

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -5,8 +5,8 @@ for i in li:
     if i <2:
         n-=1
     else:
-        for j in range(2,i):
-            if i % j == 0:
+        for j in range(2,i): #2부터 i-1까지
+            if i % j == 0:   #i를 j로 나누면서 나눠떨어지면 i가 소수 아님.
                 n-=1
                 break
 print(n)
