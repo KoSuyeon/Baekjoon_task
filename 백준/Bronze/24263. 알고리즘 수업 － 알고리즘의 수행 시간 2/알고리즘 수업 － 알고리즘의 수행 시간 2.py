f99f2d21f02ceae06295c0fd886373b3ce9078e7
kko_ba <90381286+KoSuyeon@users.noreply.github.com>
2024-08-11 03:09:09 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -0,0 +1,11 @@
+n=int(input())
+"""
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+}
+"""
+print(n) # 코드1 수행횟수: for문 돌아간만큼
+print(1) # n이므로 차수는 1
